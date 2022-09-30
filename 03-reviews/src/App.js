b4a373a76d4ef2c532bde16c59c8115048647a8a
unknown <ajfoucaultmo@mail.usf.edu>
2022-09-30 12:46:16 -0700

    03-Reviews Info And React Icons

---
+++ 03-reviews/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Review from './Review';
+import { FaGithubSquare } from 'react-icons/fa';
+function App() {
+  return (
+    <h2>
+      reviews project setup
+      <FaGithubSquare classname='icon' />
+    </h2>
+  );
+}
+
+export default App;
