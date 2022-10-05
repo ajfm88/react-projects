75d6bff71a182a995a2ef89a6dd0d3dbbddb7310
unknown <ajfoucaultmo@mail.usf.edu>
2022-10-04 20:43:06 -0700

    05-Menu All Button

--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -4,6 +4,9 @@ const Categories = ({ filterItems }) => {
   return (
     <div className='btn-container'>
       <button className='filter-btn' onClick={() => filterItems('breakfast')}>
+        all
+      </button>
+      <button className='filter-btn' onClick={() => filterItems('breakfast')}>
         breakfast
       </button>
     </div>
