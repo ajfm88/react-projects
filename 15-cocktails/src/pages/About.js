030e557ada2006da7cadc225ef2d90426c63752e
unknown <ajfoucaultmo@mail.usf.edu>
2022-10-15 11:57:15 -0700

    15-Cocktails Error About

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 classname='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem unde
+        assumenda mollitia corrupti omnis a minima cumque tempora veniam, nulla
+        magnam sapiente enim minus quos, nostrum rerum dolor quis consequatur
+        debitis adipisci similique sunt quam recusandae! Fugiat, quo!
+        Consequuntur, unde.
+      </p>
+    </section>
   );
 };
 
